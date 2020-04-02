6c5add98821e7591d02286b557f3284c3d7ee17f
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-04-02 17:53:18 +0200

    Dodaj pliki projektów.

---
+++ DeepDesign/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Calligraphy;
+using Android.Content;
+
+namespace DeepDesign
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            CalligraphyConfig.InitDefault(new CalligraphyConfig.Builder()
+                .SetDefaultFontPath("Fonts/GlacialIndifference-Regular.ttf")
+                .SetFontAttrId(Resource.Attribute.fontPath).Build());
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
+        }
+
+
+    }
+}
